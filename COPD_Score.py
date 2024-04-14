61f87fe65a5a7839d0f75126d4a794a8abb57ceb
Brbdar <94296019+Brbdar@users.noreply.github.com>
2024-04-14 12:07:56 +0200

    Add files via upload

--- COPD_Score.py
+++ COPD_Score.py
@@ -1,89 +1,150 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[23]:
 
 
 import streamlit as st
 
 def COPD_Score():
-    st.header("COPD Score")
+    import streamlit as st
+
+    st.header("##COPD Klassifizieren")
     
-    st.write("""
-        ### Bewertung der Evidenz
+    st.subheader("mMRC Dyspnoe-Skala")
+    st.write("Bitte bewerten Sie Ihren Grad der Atemnot anhand der folgenden Beschreibungen. Wählen Sie die Beschreibung, die am besten zu Ihrer aktuellen Erfahrung passt.")
 
-        Der COPD-Bewertungstest wurde ursprünglich in einer Kohorte von 1.503 Patienten entwickelt, die 6 Länder repräsentieren (Jones 2009). Es wurde gezeigt, dass er ein zuverlässiges Maß für die Gesamtschwere der COPD aus der Perspektive des Patienten bietet, unabhängig von der Sprache.
-
-        Hinsichtlich der Validität wurde auch gezeigt, dass er sowohl mit der Mortalität (Husebø 2016 fand ein Hazard-Ratio von 1,24 (1,05-1,37, p<0,01) für jede 8-Punkte-Erhöhung) als auch mit den Ergebnissen in der pulmonalen Rehabilitation korreliert (Dodd 2011). Es besteht auch eine hohe Korrelation mit dem St. George’s Respiratory Questionnaire (r = 0,84 über sieben europäische Länder (Jones 2011) und r = 0,80 in den USA (Jones 2009)).
-
-        Derzeit wird Forschung betrieben, um Bereiche der CAT-Score-Schwere zu definieren und ein besseres Verständnis für die minimal klinisch relevante Änderung eines CAT-Scores von einem Besuch zum nächsten zu erlangen.
-        """)
+    mMRC_options = [
+        "0 - Nur bei anstrengender Bewegung",
+        "1 - Bei Eile auf ebenem Grund oder beim leichten Anstieg",
+        "2 - Geht langsamer als Gleichaltrige auf ebenem Grund oder muss zum Atmen anhalten",
+        "3 - Muss nach 100 Metern oder nach wenigen Minuten auf ebenem Grund anhalten",
+        "4 - Zu atemlos, um das Haus zu verlassen oder atemlos beim Anziehen"
+    ]
+    mMRC_score = st.selectbox("Wählen Sie Ihren Grad der Atemnot:", mMRC_options, key="mMRC_score_selector")
     
-    with st.expander("Überblick und Messqualitäten"):
-        st.write("""
-        Diese Studie hat einen kurzen, einfachen, von Patienten ausgefüllten Fragebogen für COPD mit sehr guten Messqualitäten erstellt. Trotz der geringen Anzahl von Komponenten deckt er eine breite Palette von Auswirkungen der COPD auf die Gesundheit der Patienten ab. Die Qualität der Anpassung an ein Rasch eindimensionales Modell deutet darauf hin, dass es echte Intervallskalierungseigenschaften besitzt. Basierend auf Daten aus sechs Ländern zeigen Tests der internen Konsistenz, dass es ein zuverlässiges Maß für die Gesamtschwere der COPD aus der Perspektive des Patienten bietet, unabhängig von der Sprache.
-    """)
+    # Umwandlung der Auswahl in einen Punktwert
+    mMRC_score_index = mMRC_options.index(mMRC_score)  # Integer-Wert der Auswahl
 
-    with st.expander("Entwicklungsprozess und Item-Reduktion"):
-        st.write("""
-        Der Prozess der Item-Reduktion folgte einer strengen Methodik, die klassische Testtheorie und IRT kombinierte, zusammen mit sorgfältiger Überwachung des Inhalts. Items mit schlechten Messqualitäten wurden entfernt, während eine breite Abdeckung der verschiedenen Auswirkungen der COPD erhalten blieb. Bei der Entscheidung, ob ein Item während der Fragebogenentwicklung einbezogen oder ausgeschlossen werden sollte, war es notwendig, seine Schwächen und Stärken gegenüber seinem Gesamtbeitrag abzuwägen. Ein Item wurde nicht aufgrund eines einzelnen Kriteriums ausgeschlossen, sondern wegen seiner Gesamtleistung im Vergleich zu den anderen. Entwickler- und klinisches Urteil waren in diesem Prozess erforderlich.
-        """)
+    st.header("COPD Klassifizieren")
+    st.subheader("CAT-Score")
 
-    with st.expander("Endgültiger Inhalt und Format"):
-        st.write("""
-        Der endgültige Inhalt umfasst Husten, Schleim, Brustenge, Atemnot beim Aufsteigen von Hügeln/Treppen, Aktivitätsbeschränkung zu Hause, Vertrauen, das Haus zu verlassen, Schlaf und Energie. Das Prinzip, dass das Instrument zuverlässige Messqualitäten haben sollte, mit allen Items, die strenge statistische Anforderungen erfüllen, wurde erreicht. Der endgültige CAT besteht aus acht Items, die jeweils als semantische sechspunktige Differenzialskala formatiert sind, was das Werkzeug leicht zu administrieren und für Patienten leicht auszufüllen macht. Die Items wurden ausgewählt, um eine breite Palette von Krankheitsschweregraden abzudecken.
-        """)
-
-    with st.expander("Limitationen und Validierung"):
-        st.write("""
-        Eine Einschränkung dieser Studie ist, dass die Zuverlässigkeits- und Validierungsbefunde nur auf Daten aus den USA basieren. Es ist jedoch klar, dass der CAT sehr ähnliche diskriminative Eigenschaften wie der wesentlich komplexere SGRQ-C aufweist, was zeigt, dass er in der Lage sein wird, die Auswirkungen der COPD auf die Gesundheit einzelner Patienten zu messen. Die Validierung eines Instruments ist ein kontinuierlicher Prozess, und internationale Studien werden durchgeführt, um seine psychometrischen Eigenschaften weiter zu testen. Der CAT wird Klinikern und Patienten ein einfaches und zuverlässiges Maß für den allgemeinen gesundheitlichen Status im Zusammenhang mit COPD zur Beurteilung und langfristigen Nachverfolgung einzelner Patienten bieten.
-        """)
-
-    # Titel der Webanwendung
-    st.title("CAT-Score Rechner für Lungenerkrankungen")
-
-    # Symptome und deren Beschreibungen
     symptome = {
-        "Husten": "0 ('Ich huste nie') bis 5 ('Ich huste ständig')",
-        "Schleim": "0 ('Ich habe überhaupt keinen Schleim in der Brust') bis 5 ('Meine Brust ist vollständig mit Schleim gefüllt')",
-        "Brustenge": "0 ('Meine Brust fühlt sich überhaupt nicht eng an') bis 5 ('Meine Brust fühlt sich sehr eng an')",
-        "Atemnot": "0 ('Beim Gehen auf einen Hügel oder eine Treppe bekomme ich keine Luftnot') bis 5 ('Beim Gehen auf einen Hügel oder eine Treppe bin ich sehr kurzatmig')",
-        "Aktivitäten": "0 ('Ich bin bei keiner Aktivität zu Hause eingeschränkt') bis 5 ('Ich bin bei Aktivitäten zu Hause sehr eingeschränkt')",
-        "Vertrauen": "0 ('Ich bin zuversichtlich, mein Haus trotz meiner Lungenerkrankung zu verlassen') bis 5 ('Ich habe kein Vertrauen, mein Haus wegen meiner Lungenerkrankung zu verlassen')",
-        "Schlaf": "0 ('Ich schlafe fest') bis 5 ('Ich schlafe wegen meiner Lungenerkrankung nicht fest')",
-        "Energie": "0 ('Ich habe viel Energie') bis 5 ('Ich habe überhaupt keine Energie')"
+    "Husten": ["nie", "selten", "gelegentlich", "regelmäßig", "häufig", "ständig"],
+    "Schleim": ["kein", "wenig", "manchmal", "oft", "viel", "vollständig"],
+    "Brustenge": ["frei", "leicht", "gelegentlich", "oft", "stark", "sehr stark"],
+    "Atemnot": ["keine", "leicht", "mäßig", "mäßig+", "stark", "sehr stark"],
+    "Aktivitäten": ["keine", "leicht", "moderat", "deutlich", "stark", "vollständig"],
+    "Vertrauen": ["voll", "größtenteils", "leicht unsicher", "oft unsicher", "stark unsicher", "keines"],
+    "Schlaf": ["fest", "leicht gestört", "gelegentlich gestört", "oft gestört", "stark gestört", "sehr schlecht"],
+    "Energie": ["viel", "gut", "mäßig", "wenig", "sehr wenig", "keine"]
     }
 
-    # Slider für jedes Symptom
-    scores = {symptom: st.slider(f"{symptom} ({beschreibung})", min_value=0, max_value=5, value=0) for symptom, beschreibung in symptome.items()}
+    scores = {}
+    for symptom, beschreibungen in symptome.items():
+        selected_option = st.radio(
+            f"{symptom}:",
+            options=beschreibungen,
+            horizontal=True,
+            key=symptom  # Eindeutiger Schlüssel für jedes radio
+        )
+        scores[symptom] = beschreibungen.index(selected_option)  # Speichern des Index als Score
 
     # Berechnung der Gesamtpunktzahl
     total_score = sum(scores.values())
+    st.write("### Gesamtpunktzahl Ihrer Symptome: ", total_score)
 
-    # Interpretation des CAT-Scores
-    if total_score <= 10:
-        health_impact = "Niedrig"
-        recommendation = "Raucherentwöhnung, präventive Pflege und Reduzierung der Expositionsrisikofaktoren; LAMA und Notfallinhalatoren in Betracht ziehen"
-    elif total_score <= 20:
-        health_impact = "Mittel"
-        recommendation = "Raucherentwöhnung, präventive Pflege, Reduzierung der Expositionsrisikofaktoren, und LAMA und Notfallinhalatoren; ICS und/oder LABA, Überweisungen zur Lungensanierung, und mögliche Bewertung der Lungentransplantation in Betracht ziehen"
-    elif total_score <= 30:
-        health_impact = "Hoch"
-        recommendation = "Raucherentwöhnung, präventive Pflege, Reduzierung der Expositionsrisikofaktoren, ICS/LABA/LAMA-Therapie, Überweisungen zur Lungensanierung, mögliche Bewertung der Lungentransplantation und O₂-Supplementierung"
+
+
+    # Exazerbationshistorie
+    exacerbation_history = st.number_input("Anzahl der Exazerbationen im letzten Jahr:", min_value=0)
+
+    # Klassifizierung in COPD-Gruppen gemäß den neuen ABE-Leitlinien
+    if (exacerbation_history <= 1 and mMRC_score_index < 2 and total_score < 10):
+        copd_group = 'A'
+        initial_therapy = "Monotherapie mit einem Bronchodilatator (LAMA oder LABA)"
+    elif (mMRC_score_index >= 2 or total_score >= 10):
+        copd_group = 'B'
+        initial_therapy = "LAMA + LABA"
+    elif (exacerbation_history > 1 or (exacerbation_history == 1 and st.checkbox("Stationäre Behandlung benötigt"))):
+        copd_group = 'E'
+        initial_therapy = "LAMA + LABA und ggf. ICS bei Asthma in der Vorgeschichte und/oder Eosinophilenzahlen von ≥300/μL"
     else:
-        health_impact = "Sehr hoch"
-        recommendation = "Spezifische Empfehlungen basierend auf individueller Bewertung"
+        copd_group = 'Unbestimmt'  # Sicherheitsmaßnahme für ungewöhnliche Fälle
+        initial_therapy = "Bitte überprüfen Sie die Eingaben und Leitlinien."
 
-    # Anzeige des Ergebnisses
-    st.write(f"### CAT-Score: {total_score}")
-    st.write(f"### Gesundheitliche Auswirkung: {health_impact}")
-    st.write(f"### Empfehlung: {recommendation}")
+    # Anzeige der COPD-Gruppe, Initialtherapie und weitere Empfehlungen
+    st.write(f"### COPD-Gruppe: {copd_group}")
+    st.write(f"### Empfohlene Initialtherapie: {initial_therapy}")
+    st.write("### Weiterführende Empfehlungen basieren auf der Eskalationstherapie, abhängig von den spezifischen Anforderungen und dem Verlauf der Erkrankung.")
+    st.write("Bitte beachten Sie, dass diese Bewertungen auf Ihren Angaben basieren und eine professionelle medizinische Bewertung nicht ersetzen können.")
+
     
+
+    st.subheader("Eskalationstherapie")
+
+    dyspnoe_focus = st.checkbox("Steht Dyspnoe im Vordergrund?")
+    exacerbation_focus = st.checkbox("Stehen Exazerbationen im Vordergrund?")
+    prev_treatment = st.selectbox(
+        "Vorbehandlung auswählen:",
+        ["Keine Vorbehandlung", "LABA", "LAMA", "LABA + LAMA", "LAMA + ICS"]
+    )
+    eosinophils = st.number_input("Eosinophilenzahl (falls bekannt):", min_value=0, step=10, format="%d")
+
+    if dyspnoe_focus:
+        if prev_treatment in ["LABA", "LAMA"]:
+            st.success("Empfehlung: Erweitern auf LAMA + LABA.")
+        elif prev_treatment == "LABA + LAMA":
+            st.warning("Empfehlung: Wirkstoff- und/oder Inhalatorwechsel erwägen.")
+
+    if exacerbation_focus:
+        if prev_treatment in ["LABA", "LAMA"]:
+            st.success("Empfehlung: Erweitern auf LAMA + LABA.")
+        elif prev_treatment == "LAMA + LABA" and eosinophils >= 100:
+            st.success("Empfehlung: Erweitern auf LAMA + LABA + ICS.")
+        elif prev_treatment == "LAMA + LABA" and eosinophils < 100:
+            st.info("Weitere Optionen zur Therapieeskalation prüfen.")
+        elif prev_treatment == "LAMA + ICS":
+            if eosinophils >= 300:
+                st.success("Empfehlung: Umstellung auf LAMA + LABA + ICS.")
+            else:
+                st.info("Bei guter Therapieeinstellung: Beibehalten möglich. Bei unzureichendem Ansprechen: Umstellung erwägen.")
+
+    fev1 = st.number_input("FEV1 in % (falls bekannt):", min_value=0, max_value=100, step=1, format="%d")
+    chronic_bronchitis = st.checkbox("Chronische Bronchitis?")
+    hospitalization_last_year = st.checkbox("Mindestens eine Hospitalisierung wegen COPD im Vorjahr?")
+
+    if fev1 < 50 and chronic_bronchitis and hospitalization_last_year:
+        st.error("Empfehlung: Roflumilast in Erwägung ziehen.")
+
+    if exacerbation_focus:
+        st.success("Empfehlung: Azithromycin in Erwägung ziehen, falls entsprechend indiziert.")
+
+    st.info("""
+    **Wichtige Informationen:**
+    - Von einer Azithromycin-Gabe profitieren v.a. Patient:innen, die nicht mehr rauchen. Allerdings sollte die Gefahr der Resistenzentwicklung bei längerfristiger Gabe in die Therapieentscheidung mit einbezogen werden.
+    - Bei Patienten mit obstruktiven Atemwegserkrankungen wie Asthma und COPD soll eine Therapie mit Inhalatoren nicht begonnen oder geändert werden, ohne dass der Patient im Gebrauch des Inhalationssystems geschult ist und die korrekte Anwendung der Inhalatoren überprüft wurde.
+    """)
+
     st.markdown("""
     Development and first validation of the COPD Assessment Test
 
     P. W. Jones, G. Harding, P. Berry, I. Wiklund, W-H. Chen, N. Kline Leidy  
     *European Respiratory Journal* 2009 34: 648-654; DOI: [10.1183/09031936.00102509](https://erj.ersjournals.com/content/34/3/648.long)
     """)
+    
+    st.markdown("""
+    **Verteilung und prognostische Validität der neuen Einstufung nach der Global Initiative for Chronic Obstructive Lung Disease**
+
+    Joan B Soriano  1 , Inmaculada Alfageme  2 , Pere Almagro  3 , Ciro Casanova  4 , Cristobal Esteban  5 , Juan J Soler-Cataluña  6 , Juan P de Torres  7 , Pablo Martinez-Camblor  8 , Marc Miravitlles  9 , Bartolome R Celli  10 , Jose M Marin  11
+    *Affiliationen erweitern*
+    PMID: 23187891  DOI: [10.1378/chest.12-1053](https://pubmed.ncbi.nlm.nih.gov/23187891/)
+    """)
+
+
+
+# In[ ]:
+
+
 
 
